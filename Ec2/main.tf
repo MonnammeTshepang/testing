7bd4bd6694f7cc3b6495da176bd78495214ea17c
TSHEPANG <tshepangd08@gmail.com>
2023-04-04 11:10:50 +0200

    bucket

--- Ec2/main.tf
+++ Ec2/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region     = "eu-central-1"
-  profile    = "AWSprofile1"
+  region  = "eu-central-1"
+  profile = "AWSprofile1"
 }
 
 # 1. Create VPC
@@ -30,8 +30,8 @@ resource "aws_route_table" "my-route-table" {
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.VPCgateway.id
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.VPCgateway.id
   }
 
   tags = {
@@ -60,33 +60,33 @@ resource "aws_security_group" "allow_ingress-egress" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    description      = "HTTPS traffic"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
   ingress {
-    description      = "HTTP traffic"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description      = "SSH traffic"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -104,7 +104,7 @@ resource "aws_eip" "elastic_IP" {
   vpc                       = true
   network_interface         = aws_network_interface.interface1.id
   associate_with_private_ip = "172.16.10.30"
-  depends_on = [aws_internet_gateway.VPCgateway]
+  depends_on                = [aws_internet_gateway.VPCgateway]
 }
 # 9. Create Ubuntu server and install/enable apache2
 resource "aws_instance" "instance-1" {
